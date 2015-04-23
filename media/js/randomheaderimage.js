5ddf65af38f7872e3961c9a9f6874ac422085446
calvin butcher <calvin.butcher@bodleian.ox.ac.uk>
2015-04-23 13:19:13 +0100

    added debug window

--- media/js/randomheaderimage.js
+++ media/js/randomheaderimage.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+	alert('aloha!');
     var header = document.getElementById('header');
     var pictures = new Array('/mlgb/media/img/bannerimages/bannerimage1.jpg','/mlgb/media/img/bannerimages/bannerimage2.jpg','/mlgb/media/img/bannerimages/bannerimage3.jpg','/mlgb/media/img/bannerimages/bannerimage4.jpg','/mlgb/media/img/bannerimages/bannerimage5.jpg','/mlgb/media/img/bannerimages/bannerimage6.jpg','/mlgb/media/img/bannerimages/bannerimage7.jpg','/mlgb/media/img/bannerimages/bannerimage8.jpg',);
     var numPics = pictures.length;
