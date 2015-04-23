40aa690de371c844273d615d106f45de3889553c
calvin butcher <calvin.butcher@bodleian.ox.ac.uk>
2015-04-23 12:22:12 +0100

    updated banner rotate js

--- media/js/randomheaderimage.js
+++ media/js/randomheaderimage.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     var header = document.getElementById('header');
-    var pictures = new Array('images/image1.jpg','images/image2.jpg','images/image3.jpg','images/image4.jpg','images/image5.jpg');
+    var pictures = new Array('/static/media/img/bannerimages/bannerimage1.jpg','/static/media/img/bannerimages/bannerimage2.jpg','/static/media/img/bannerimages/bannerimage3.jpg','/static/media/img/bannerimages/bannerimage4.jpg','/static/media/img/bannerimages/bannerimage5.jpg','/static/media/img/bannerimages/bannerimage6.jpg','/static/media/img/bannerimages/bannerimage7.jpg','/static/media/img/bannerimages/bannerimage8.jpg',);
     var numPics = pictures.length;
     if (document.images) {
         var chosenPic = Math.floor((Math.random() * numPics));
