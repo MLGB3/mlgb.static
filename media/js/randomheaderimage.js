745af4605b8a2f715234919359ac1d092e12a6c2
calvin butcher <calvin.butcher@bodleian.ox.ac.uk>
2015-04-23 12:25:57 +0100

    corrected image paths

--- media/js/randomheaderimage.js
+++ media/js/randomheaderimage.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     var header = document.getElementById('header');
-    var pictures = new Array('/static/media/img/bannerimages/bannerimage1.jpg','/static/media/img/bannerimages/bannerimage2.jpg','/static/media/img/bannerimages/bannerimage3.jpg','/static/media/img/bannerimages/bannerimage4.jpg','/static/media/img/bannerimages/bannerimage5.jpg','/static/media/img/bannerimages/bannerimage6.jpg','/static/media/img/bannerimages/bannerimage7.jpg','/static/media/img/bannerimages/bannerimage8.jpg',);
+    var pictures = new Array('/mlgb/media/img/bannerimages/bannerimage1.jpg','/mlgb/media/img/bannerimages/bannerimage2.jpg','/mlgb/media/img/bannerimages/bannerimage3.jpg','/mlgb/media/img/bannerimages/bannerimage4.jpg','/mlgb/media/img/bannerimages/bannerimage5.jpg','/mlgb/media/img/bannerimages/bannerimage6.jpg','/mlgb/media/img/bannerimages/bannerimage7.jpg','/mlgb/media/img/bannerimages/bannerimage8.jpg',);
     var numPics = pictures.length;
     if (document.images) {
         var chosenPic = Math.floor((Math.random() * numPics));
