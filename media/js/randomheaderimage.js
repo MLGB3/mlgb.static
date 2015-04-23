fd4d29383a1741ac59160abcdf8021beec7eae04
calvin butcher <calvin.butcher@bodleian.ox.ac.uk>
2015-04-23 14:50:02 +0100

    test

--- media/js/randomheaderimage.js
+++ media/js/randomheaderimage.js
@@ -1,4 +1,4 @@
-document.ready() {
+$(document).ready(function( {
 	alert('aloha!');
     var header = document.getElementById('header');
     var pictures = new Array('/mlgb/media/img/bannerimages/bannerimage1.jpg','/mlgb/media/img/bannerimages/bannerimage2.jpg','/mlgb/media/img/bannerimages/bannerimage3.jpg','/mlgb/media/img/bannerimages/bannerimage4.jpg','/mlgb/media/img/bannerimages/bannerimage5.jpg','/mlgb/media/img/bannerimages/bannerimage6.jpg','/mlgb/media/img/bannerimages/bannerimage7.jpg','/mlgb/media/img/bannerimages/bannerimage8.jpg',);
@@ -6,4 +6,4 @@ document.ready() {
     var chosenPic = Math.floor((Math.random() * numPics));
     header.style.background = 'url(' + pictures[chosenPic] + ')';
    
-}
+}));
